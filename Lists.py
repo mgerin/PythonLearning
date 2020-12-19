68d39353f0c212a08b2967f32819cab1b5f0153d
mgerin <marin_gerin@abv.bg>
2020-12-19 17:14:31 +0200

    Lists exercises

--- Lists.py
+++ Lists.py
@@ -46,3 +46,30 @@ print(list2)
 print(sorted(list2, reverse=True))
 print(list1 + list2)
 print(list2 * 3)
+
+#Coding task 1
+my_list = [22, 109, 'Apple', 11.2, 'Orange', 'Cherry', (10, 20, 30), [], False, 72, 'Kiwi', 0.0]
+
+x = my_list[6:8]
+
+print(x)
+#Coding task 2
+my_list = [10, 'x', 20.02, 'y', 30j, 'z', False]
+
+my_list.append("Python 3")
+
+print(my_list)
+#Coding task 3
+my_list = [10, 'x', 20.02, 'y', 30j, 'z', False]
+
+del my_list[2]
+my_list.remove("x")
+
+print(my_list)
+
+#Coding task 4
+my_list = [10, -5, 20.02, 2018, 999]
+
+my_list.sort()
+
+print(my_list)

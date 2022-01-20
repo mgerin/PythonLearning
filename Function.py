97938e837a40426946da251938a5d635b8744ae9
mgerin <marin_gerin@abv.bg>
2022-01-20 17:33:45 +0200

    Functions and Imports

--- Function.py
+++ Function.py
@@ -1,17 +1,80 @@
-#Functions
+# Functions
 def say_hi(name, age):
     print("Hello " + name + ", you are " + str(age) + " years old")
+
 
 say_hi("Marin", 32)
 say_hi("Lyubo", 30)
 
+
 def cube(num):
     return num*num*num
+
 
 result = cube(5)
 print(result)
 
-#If statements
+
+def my_first_function(x, y, z):
+    sum_xyz = (x + y) * z
+    return sum_xyz
+
+
+my_first_function(1, 2, 3)
+my_first_function(x = 1, z = 3, y = 2)
+
+
+def my_second_function(x, y, z = 3):
+    sum_xyz = (x + y) * z
+    return sum_xyz
+
+
+my_second_function(1, 2)
+
+
+def args_function(x, *args):
+    print(x)
+    for argument in args:
+        print(argument)
+
+
+args_function("hello")
+args_function("hello", 100, 200)
+
+
+def my_var_func():
+    my_var = 10
+    print(my_var)
+
+
+my_var_func()
+my_var = 20
+print(my_var * 10)
+
+
+my_var1 = 5
+
+
+def my_new_var_func():
+    global my_var1
+    print(my_var1)
+    my_var1 = 10
+
+
+my_new_var_func()
+
+
+def my_third_func():
+    my_new_var = 10
+    print(my_new_var)
+    return my_new_var
+
+
+result = my_third_func()
+
+print(result * 10)
+
+# If statements
 is_male = True
 is_tall = False
 
---
+++ my_module.py
@@ -0,0 +1,11 @@
+my_var = 10
+
+
+def my_function():
+    print("This is the function inside the module!")
+
+
+print("this will be executed")
+
+if __name__ == "__main__":
+    print("This won't be executed")
